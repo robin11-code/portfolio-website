d1eb1be1f064733c4c83d9bece67a2cbc99bc7f2
Robin Bose <63658482+robin11-code@users.noreply.github.com>
2026-05-17 01:51:43 +0530

    Duplicate file removal
    Duplicate file removal

--- artifacts/portfolio/src/App.tsx
+++ artifacts/portfolio/src/App.tsx
@@ -1,6 +1,4 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
@@ -15,12 +13,9 @@ function Router() {
 
 function App() {
   return (
-    <TooltipProvider>
-      <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
-        <Router />
-      </WouterRouter>
-      <Toaster />
-    </TooltipProvider>
+    <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+      <Router />
+    </WouterRouter>
   );
 }
 
